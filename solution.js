48f6ad6b367285788aa84d915301fbaef9d3310a
octoccoper <octocopper@gmail.com>
2019-06-28 20:43:21 +0300

    removed console.log() functions

--- solution.js
+++ solution.js
@@ -1,4 +1,41 @@
 function calc(firstNumber, secondNumber, operation, result) {
-  // ваше решение пишите здесь
-  return eval(`(${firstNumber} ${operation} ${secondNumber}) === ${result}`);
+  let operationResult;
+
+  if (typeof firstNumber === "string") {
+    firstNumber = parseFloat(firstNumber);
+  }
+
+  if (typeof secondNumber === "string") {
+    secondNumber = parseFloat(secondNumber);
+  }
+
+  if (typeof result === "string") {
+    result = parseFloat(result);
+  }
+
+  if (firstNumber > 1000000 || secondNumber > 1000000) {
+    console.log(
+      "You entered numbers more than 1000000, please enter less number."
+    );
+    return;
+  }
+
+  switch (operation) {
+    case "+":
+      operationResult = firstNumber + secondNumber;
+      break;
+    case "-":
+      operationResult = firstNumber - secondNumber;
+      break;
+    case "/":
+      operationResult = firstNumber / secondNumber;
+      break;
+    case "*":
+      operationResult = firstNumber * secondNumber;
+      break;
+    default:
+      console.log("Sorry, we are out of " + operation + ".");
+  }
+
+  return operationResult === result;
 }
